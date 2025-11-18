175d5a67c6f84d23ae17dff4d5a73b718c4f2f04
Akash <akash.bhuiyan@gmail.com>
2025-11-18 16:14:58 +0100

    Initial project added

---
+++ src/main/java/com/akash/food_inventory_service/FoodInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.akash.food_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akash/food_inventory/FoodInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.food_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
